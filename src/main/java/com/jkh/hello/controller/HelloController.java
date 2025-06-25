2f90dbd089b46c75caf2ad01cfaefa296b50fabb
김진현 <wlsgus63@naver.com>
2025-06-25 15:49:15 +0900

    [Feat]: "/" page 영어 퀴즈 기능

--- src/main/java/com/jkh/hello/controller/HelloController.java
+++ src/main/java/com/jkh/hello/controller/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    @GetMapping("/")
+    public String first() {
+        return "index";
+    }
 
     @GetMapping("hello")
     public String hello() {
